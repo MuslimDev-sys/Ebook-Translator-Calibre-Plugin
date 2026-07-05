9ef17e1cc90230094e938fd138040684a3807331
MuslimDev-sys <main.business.email@proton.me>
2026-07-05 22:01:39 +0300

    Update placeholder formatting in base.py

--- engines/base.py
+++ engines/base.py
@@ -33,7 +33,7 @@ class Base:
     api_key_errors = ['401']
     separator = '\n\n'
     support_html = False
-    placeholder = ('__id_{}__', r'__\s*id\s*_\s*{}\s*__')
+    placeholder = (' __id_{}__ ', r'\s*__\s*id\s*_\s*{}\s*__\s*')
     using_tip = None
 
     concurrency_limit: int = 0
